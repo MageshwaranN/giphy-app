b5a74ef8cd87c80566e167b5d826f6d1a7b314bd
Nathan <mageshwaran.nathan@aegon.nl>
2020-01-23 21:54:22 +0100

    fixing the default tests

--- src/app/shared/services/giphy/giphy-search.service.spec.ts
+++ src/app/shared/services/giphy/giphy-search.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GiphySearchService } from './giphy-search.service';
+import { HttpClientTestingModule } from '../../../../../node_modules/@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 describe('GiphySearchService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule ],
+    providers: [HttpClient]
+  }));
 
   it('should be created', () => {
     const service: GiphySearchService = TestBed.get(GiphySearchService);
